1e90b9eae8f2d91995e5300c4f63fd01d93132b2
karank28 <199122894+kmistry-exe@users.noreply.github.com>
2026-02-08 13:57:23 +0530

    feat(admin): added admin layout with nested routing and sidebar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { AdminLayoutComponent } from './layout/admin-layout/admin-layout.component';
+import { DashboardComponent } from './pages/admin/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'admin',
+        component: AdminLayoutComponent,
+        children: [
+            {
+                path: 'dashboard',
+                component: DashboardComponent
+            },{
+                path: '',
+                redirectTo: 'dashboard',
+                pathMatch: 'full'
+            }
+        ]
+    },
+    {
+        path: '',
+        redirectTo: 'admin',
+        pathMatch: 'full'
+    }
+];
