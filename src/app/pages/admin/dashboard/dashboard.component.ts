dc682c4dc97e254de862fb1cb5c053bbfedef1cd
karank28 <199122894+kmistry-exe@users.noreply.github.com>
2026-02-09 23:58:32 +0530

    feat(dashboard): add initial stats section UI

--- src/app/pages/admin/dashboard/dashboard.component.ts
+++ src/app/pages/admin/dashboard/dashboard.component.ts
@@ -1,11 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
 
+export class DashboardComponent {
+  stats = [
+    { label: 'Total Orders', value: 1245 },
+    { label: 'Revenue', value: 82400, prefix: '₹' },
+    { label: 'Customers', value: 540 },
+    { label: 'Products', value: 128 },
+  ];
 }
