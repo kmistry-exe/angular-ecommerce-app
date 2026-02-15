d66b7c5ea21846b0264d73d5ba28984f67f3eb8f
karank28 <199122894+kmistry-exe@users.noreply.github.com>
2026-02-15 22:11:53 +0530

    refactor(products): convert edit product form to reactive forms with patchValue

--- src/app/pages/admin/products/add-products/add-products.component.ts
+++ src/app/pages/admin/products/add-products/add-products.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-add-products',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './add-products.component.html',
   styleUrl: './add-products.component.css'
 })
-export class AddProductsComponent {
 
+export class AddProductsComponent {
+  productForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.productForm = this.fb.group({
+      name: [''],
+      category: [''],
+      price: [''],
+      stock: [''],
+      description: ['']
+    });
+  }
 }
--- src/app/pages/admin/products/edit-product/edit-product.component.ts
+++ src/app/pages/admin/products/edit-product/edit-product.component.ts
@@ -1,21 +1,19 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-product',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './edit-product.component.html',
   styleUrl: './edit-product.component.css'
 })
 export class EditProductComponent {
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private fb: FormBuilder) { }
 
   productId!: number;
-
-  ngOnInit(): void {
-    this.productId = Number(this.route.snapshot.paramMap.get('id'));
-    console.log('Editing product ID:', this.productId);
-  }
+  productForm!: FormGroup;
 
   mockProduct = {
     name: 'Sample Product',
@@ -25,4 +23,19 @@ export class EditProductComponent {
     description: 'This is a sample product description'
   };
 
+  ngOnInit(): void {
+    this.productForm = this.fb.group({
+      name: [''],
+      category: [''],
+      price: [''],
+      stock: [''],
+      description: ['']
+    });
+
+    this.productId = Number(this.route.snapshot.paramMap.get('id'));
+
+    if (this.productId) {
+      this.productForm.patchValue(this.mockProduct);
+    }
+  }
 }
