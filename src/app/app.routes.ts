d13f513274139985404eb2fee0ba0a182588b0b8
karank28 <199122894+kmistry-exe@users.noreply.github.com>
2026-02-27 23:37:53 +0530

    feat(auth): implement admin login, logout, auth service and route guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,19 @@ import { DashboardComponent } from './pages/admin/dashboard/dashboard.component'
 import { ProductsListComponent } from './pages/admin/products/products-list/products-list.component';
 import { AddProductsComponent } from './pages/admin/products/add-products/add-products.component';
 import { EditProductComponent } from './pages/admin/products/edit-product/edit-product.component';
+import { LoginComponent } from './pages/auth/login/login.component';
+import { authGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
     {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
         path: 'admin',
         component: AdminLayoutComponent,
+        canActivate: [authGuard],
+        canActivateChild: [authGuard],
         children: [
             {
                 path: 'dashboard',
@@ -31,7 +39,7 @@ export const routes: Routes = [
     },
     {
         path: '',
-        redirectTo: 'admin',
+        redirectTo: 'login',
         pathMatch: 'full'
     }
 ];
--- src/app/layout/admin-layout/admin-layout.component.ts
+++ src/app/layout/admin-layout/admin-layout.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { AuthService } from '../../core/services/auth.service';
 
 @Component({
   selector: 'app-admin-layout',
@@ -7,6 +8,12 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
   templateUrl: './admin-layout.component.html',
   styleUrl: './admin-layout.component.css'
 })
+
 export class AdminLayoutComponent {
 
+  constructor(private authService: AuthService) { }
+
+  logout(): void {
+    this.authService.logout();
+  }
 }
