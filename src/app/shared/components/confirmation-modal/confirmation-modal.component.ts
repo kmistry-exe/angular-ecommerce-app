edaa3f9b07c9db625a02ed48c78a845183e0aafa
Karan Mistry <karanmistry2804@gmail.com>
2026-03-08 20:47:39 +0530

    feat(ui): implement reusable confirmation modal and fixed bug of edit modal.

---
+++ src/app/shared/components/confirmation-modal/confirmation-modal.component.ts
@@ -0,0 +1,33 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InputButtonComponent } from '../input-button/input-button.component';
+import { PageHeaderComponent } from '../page-header/page-header.component';
+
+@Component({
+  selector: 'app-confirmation-modal',
+  imports: [CommonModule, InputButtonComponent, PageHeaderComponent],
+  templateUrl: './confirmation-modal.component.html',
+})
+export class ConfirmationModalComponent {
+  @Input() title = 'Confirm Action';
+  @Input() message = 'Are you sure you want to proceed?';
+
+  @Input() confirmText = 'Confirm';
+  @Input() cancelText = 'Cancel';
+
+  @Input() confirmVariant: 'primary' | 'danger' = 'primary';
+
+  @Output() confirm = new EventEmitter<void>();
+  @Output() cancel = new EventEmitter<void>();
+
+  @Input() cancelIcon?: string = 'fa-xmark';
+  @Input() confirmIcon?: string = 'fa-check';
+
+  onConfirm() {
+    this.confirm.emit();
+  }
+
+  onCancel() {
+    this.cancel.emit();
+  }
+}
