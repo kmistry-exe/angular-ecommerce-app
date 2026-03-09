fc376d73d930188461264afe058e8c1652c08f4f
Karan Mistry <199122894+kmistry-exe@users.noreply.github.com>
2026-03-09 22:19:39 +0530

    fix(auth-ui): resolve login/logout issues and improve UI consistency

--- src/app/layout/admin-layout/admin-layout.component.ts
+++ src/app/layout/admin-layout/admin-layout.component.ts
@@ -2,17 +2,40 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { AuthService } from '../../core/services/auth.service';
 import { InputButtonComponent } from '../../shared/components/input-button/input-button.component';
+import { ConfirmationModalComponent } from '../../shared/components/confirmation-modal/confirmation-modal.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-admin-layout',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, InputButtonComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    InputButtonComponent,
+    ConfirmationModalComponent,
+  ],
   templateUrl: './admin-layout.component.html',
   styleUrl: './admin-layout.component.css',
 })
 export class AdminLayoutComponent {
+  showLogoutConfirm = false;
   constructor(private authService: AuthService) {}
 
   logout(): void {
     this.authService.logout();
+  }
+
+  openLogoutConfirm(): void {
+    this.showLogoutConfirm = true;
+  }
+
+  confirmLogout(): void {
+    this.showLogoutConfirm = false;
+    this.authService.logout();
+  }
+
+  cancelLogout(): void {
+    this.showLogoutConfirm = false;
   }
 }
